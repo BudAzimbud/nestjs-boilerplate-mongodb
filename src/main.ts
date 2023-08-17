8b038f34e5cc5b562777420b3d90128ee94b0ca4
Azim Ms <azim.ms@ifg-life.id>
2023-08-17 22:25:55 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   console.log(process.env.URL_MONGO);
   console.log(process.env.PORT)
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('AZIM GROUP')
+    .setDescription('AZIM GROUP')
+    .setVersion('1.0')
+    .addTag('Azim GROUP')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(process.env.PORT || 8080);
 }
