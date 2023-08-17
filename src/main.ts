54f78e209481069f0cb9409f782d5577d1d46fd6
Azim Ms <azim.ms@ifg-life.id>
2023-08-17 22:11:21 +0700

    change port to 80

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(8080);
+  await app.listen(80);
 }
 bootstrap();
